17e167400f9cc24bd013ac1536a30364dbe791ad
Simon Tse <simon.tse.hk@gmail.com>
2022-12-22 23:02:11 +0800

    Let hibernate show the generated SQL in JSON logging rather than printing it in plain

--- test/Book.getBookById.sh
+++ test/Book.getBookById.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-source ../.mode.rc
-source ./config.rc
+scriptPath=$(cd $(dirname "$0") && pwd)
+
+source "$scriptPath"/../.mode.rc
+source "$scriptPath"/config.rc
 
 curl -w "\nHTTP %{http_code} time:%{time_total}s\n" -v \
 	-H "Content-Type: application/json; charset=utf-8" \
