bbe3de1c111ca6ada1f83dbdbc68e198e70d2acc
Simon Tse <simon.tse.hk@gmail.com>
2024-04-22 23:49:44 +0800

    Renamed mysql namespace

--- db/mysql/mysql-k8s-init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kubectl run mysql-client --rm --tty -i --restart='Never' --image docker.io/bitnami/mysql:8.0.31-debian-11-r10 --namespace db --env MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace db mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d) --command -- bash -c 'mysql -h mysql.db.svc.cluster.local -uroot -p"$MYSQL_ROOT_PASSWORD" -e ""'
